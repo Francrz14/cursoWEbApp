caaa785e2d00115bb76ab7397b633975263958d5
Francrz14 <ropero.lindes.fran@gmail.com>
2024-03-01 09:03:15 +0100

    modificacion

--- PHP/29-02/variables/variables.php
+++ PHP/29-02/variables/variables.php
@@ -3,24 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Variables PHP</title>
 </head>
 <body>
     <?php
         $nombre = "Fran";
         $apellido = "Ropero";
+        $apellido2 = "Lindes";
         $edad = 38;
+        $comunidad = "Jaén";
         $ciudad = "Linares";
-        echo "<h2>Hola " .$nombre. " " .$apellido. "</h2>";
+        echo "<h2>Hola " .$nombre. " " .$apellido. " " .$apellido2. "</h2>";
         echo "<h2>Edad $edad</h2>"; 
-        echo "<h2>Ciudad $ciudad </h2>"; 
+        echo "<h2>Comunidad $comunidad </h2>"; 
+        echo "<h2>Ciudad $ciudad </h2>" ."<br>"; 
     ?>
 
     <?php
     function datos(){
-        global $nombre, $apellido, $edad, $ciudad;
-        echo "<h2>Hola " .$nombre. " " .$apellido. "</h2>";
+        global $nombre, $apellido, $apellido2, $edad, $comunidad, $ciudad;
+        echo "<h2>Hola " .$nombre. " " .$apellido. " " .$apellido2. "</h2>";
         echo "<h2>Edad $edad</h2>"; 
+        echo "<h2>Comunidad $comunidad </h2>";
         echo "<h2>Ciudad $ciudad </h2>";
         exit;
     }
